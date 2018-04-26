eac09cdddc30d746c630bfa3131081a93308ed8a
apple502j <33279053+apple502j@users.noreply.github.com>
2018-04-26 17:51:37 +0900

    Version update; no support for python 2.x

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="MWSyntaxChecker",
-    version="1.0.0",
+    version="2.0.0",
     description="Checker for MediaWiki syntax",
     long_description="Checks wikitext for common errors.",
     url="https://github.com/apple502j/MWSyntaxChecker",
@@ -13,10 +13,9 @@ setup(
         'Intended Audience :: Developers',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: Wiki',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6'
     ],
     keywords='mediawiki syntax checker',
     packages=find_packages(),
-    python_requires='>=2.7',
+    python_requires='>=3.6',
 )
